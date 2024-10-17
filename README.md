9de8fe038de9fabbb81c608911a163d626f292f0
guima <gabrielguimasr@gmail.com>
2024-10-17 08:47:17 -0300

    Mask and reset-params working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # prunning_networks
-A repository to study prunning technics in neural networks
+A repository to study prunning technics in neural networks.
+My first approch is to trying find winning tickets among the neural network 
--- prunning.py
+++ prunning.py
@@ -1,26 +1,34 @@
 import torch
-
+import torch.nn.utils.prune as prune
+import numpy as np
 
 def generate_mask(model):
-    W = list(model.parameters())
-    i = 0
-
-    mask = []
-    max_minimal_value = -1e4
-    for weight in W:
-        j=0
+    
+    W_list = list(model.parameters())
+    
+    med_minimal_value = []
+    for weight in W_list:
         
-        for neuron in weight:
-            
-            if i % 2 == 0:
-                minimal = torch.min(neuron)
-                max_minimal_value = minimal.item() if minimal>max_minimal_value else max_minimal_value 
-                
-            j+=1
-                
-        i+=1
-        mask.append(torch.clone(weight).detach().ge(max_minimal_value))
-        max_minimal_value = -1e4
-
-
+        if weight.dim() > 1:
+            minimal = torch.min(weight)
+            med_minimal_value.append(minimal.item())
+    
+    
+    median_array = np.array(med_minimal_value)
+    min_median = np.median(median_array)
+    
+    
+    mask = []
+    
+    for weight in W_list:
+        if weight.dim() > 1:
+            mask.append(weight > min_median) 
+        else:
+            mask.append(torch.ones_like(weight))#always true, otherwise it will affect the bias term
+    
     return mask
+
+def apply_mask(model, mask):
+    with torch.no_grad(): 
+        for param, mask_tensor in zip(model.parameters(), mask):
+            param.data *= mask_tensor 
